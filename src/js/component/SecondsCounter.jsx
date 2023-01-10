3eec20c3367cf8524b3585da55993e13a609024f
JairzinhoTorres <jair1924torres@gmail.com>
2023-01-10 14:32:45 +0000

    Pronto el diseño del cronometro

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const SecondsCounter = (props) => {
+	return (
+		<>
+		<div className="container text-center d-flex bg-black m-auto justify-content-center">
+		<div className="row">
+		<div className="col fs-1 bg-white mt-2 mb-2">
+				<i className="fa fa-clock"></i>
+				</div>
+				<div className="col">
+		<p className="fs-1 text-white"> {props.numero6}</p>
+		</div>
+		<div className="col">
+		<p className="fs-1 text-white"> {props.numero5}</p>
+		</div>	
+		<div className="col">
+		<p className="fs-1 text-white"> {props.numero4}</p>
+		</div>	
+		<div className="col">
+		<p className="fs-1 text-white"> {props.numero3}</p>
+		</div>	
+		<div className="col">
+		<p className="fs-1 text-white"> {props.numero2}</p>
+		</div>
+			<div className="col">
+		<p className="fs-1 text-white"> {props.numero1}</p>
+		</div>
+		</div>
+		</div>
+		</>
+	);
+};
+
+export default SecondsCounter;
+
